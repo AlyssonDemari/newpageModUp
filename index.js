7cd37f429839685e6a669cfba27cd51d08aacb08
AlyssonDemari <alyssondemari@gmail.com>
2025-03-19 19:23:14 -0300

    teste 3

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
 const navMobile = document.querySelector('.nav-mobile');
-const shadow = document.querySelector('#shadow');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMobile.classList.toggle('active');
-    shadow.classList.toggle('active');
 });
 
 
