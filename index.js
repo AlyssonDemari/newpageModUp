af8ca9a8c7b581d62b2cdbdaa5dbc1348ec5a7bc
AlyssonDemari <alyssondemari@gmail.com>
2025-03-19 21:14:19 -0300

    retirando marcações

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const navMobile = document.querySelector('.nav-mobile');
+const shadow = document.querySelector('#shadow');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMobile.classList.toggle('active');
+    shadow.classList.toggle('active');
 });
 
 
