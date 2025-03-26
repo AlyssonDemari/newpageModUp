ec60002757d75284a8512ef2f2972421a70a16b3
Alysson Demari <145402961+AlyssonDemari@users.noreply.github.com>
2025-03-26 08:48:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
-const navMobile = document.querySelector('.nav-mobile');
-const shadow = document.querySelector('#shadow');
-
-const toggleMenu = () => {
-    hamburger.classList.toggle('active');
-    navMobile.classList.toggle('active');
-    shadow.classList.toggle('active');
-};
-
-
+ const navMobile = document.querySelector('.nav-mobile');
+ const shadow = document.querySelector('#shadow');
+ 
+ hamburger.addEventListener('click', () => {
+     hamburger.classList.toggle('active');
+     navMobile.classList.toggle('active');
+     shadow.classList.toggle('active');
+ });
 
 
 
